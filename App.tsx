e9dfc5fdaa1532611fc601182b896e3787ee1a69
todoranalex <alexandru.todoran@corebuild.eu>
2020-11-01 15:04:51 +0200

    chore(): add fonts

--- App.tsx
+++ App.tsx
@@ -1,30 +1,10 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import {StatusBar} from 'react-native';
 
-const App: () => JSX.Element = () => {
+export default (): JSX.Element => {
   return (
-    <>
+    <React.Fragment>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <Text>Works?</Text>
-      </SafeAreaView>
-    </>
+    </React.Fragment>
   );
 };
-
-export default App;
